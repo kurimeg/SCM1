db4dc944f873aadfeb635205a532b774a3f0d0d2
Megumi Kurihara <pomme.c5.musique@gmail.com>
2017-11-14 09:37:09 +0900

    テストGETメソッド追加

--- SCM1_Electron/src/renderer/store/modules/auth.js
+++ SCM1_Electron/src/renderer/store/modules/auth.js
@@ -1,4 +1,5 @@
 import router from '@/router'
+import axios from 'axios'
 
 const state = {
     isLogged: !!localStorage.getItem('token')
@@ -9,15 +10,26 @@ const mutations = {
         state.isLogged = true
       }
 }
-  
+
 const actions = {
-    login ({ commit }, payload) {
-        localStorage.setItem('token', 'sampletoken')
-        commit('login')
-        router.replace('chart')
-        // axios.post('/', {
-        //     empNo: payload.empNo,
-        //     password: payload.password
+    login ({ commit }, authInfo) {
+        // localStorage.setItem('token', 'sampletoken')
+        // console.log(localStorage.getItem("token"))
+        // commit('login')
+        // router.replace('chart')
+        axios.get('http://scm1test.azurewebsites.net/api/emp/', {
+            params: {
+                searchempno : 46012
+            }
+        })
+        .then((response) =>{
+            console.log(response)
+        })
+
+
+        // axios.post('', {
+        //     empNo: authInfo.empNo,
+        //     password: authInfo.password
         //   })
         //   .then((response) => {
         //     localStorage.setItem('token', 'sampletoken')
