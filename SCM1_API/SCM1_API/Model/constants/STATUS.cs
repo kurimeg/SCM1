d216c3d2e1b5048a3cf64b4d5ccad8c787ba4d54
清水 慶太 <shimizu-keita@ksc.co.jp>
2017-11-15 14:51:11 +0900

    Logger + auth API 修正

---
+++ SCM1_API/SCM1_API/Model/constants/STATUS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCM1_API.Model.constants
+{
+    public class STATUS
+    {
+        /// <summary>
+        /// 処理成功
+        /// </summary>
+        public const string OK = "OK";
+        /// <summary>
+        /// 処理失敗
+        /// </summary>
+        public const string NG = "NG";
+        /// <summary>
+        /// 異常終了（予期せぬエラー）
+        /// </summary>
+        public const string ER = "ER";
+    }
+}
--- SCM1_API/SCM1_API/PresentationService/Auth_PresentationService.cs
+++ SCM1_API/SCM1_API/PresentationService/Auth_PresentationService.cs
@@ -1,5 +1,7 @@
-﻿using SCM1_API.Model.ScreenModel.Auth;
+﻿using SCM1_API.Model.constants;
+using SCM1_API.Model.ScreenModel.Auth;
 using SCM1_API.Service;
+using SCM1_API.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,21 +23,34 @@ namespace SCM1_API.PresentationService
         {
             var resModel = new Response()
             {
+                Status = STATUS.NG,
                 Authenticated = false,
                 Token = ""
             };
             var empInfo = empService.FetchEMPInfo_Service(req.EmpNo);
 
             if (empInfo == null || empInfo.Count() > 1)
+            {
+                Logger.WriteError("社員情報の取得件数が正しくありません。");
                 return resModel;
+            }
 
             if (req.Password != empInfo.First().LOGIN_PASSWORD)
+            {
+                Logger.WriteError("社員番号/パスワードの組み合わせが適切ではありません。");
                 return resModel;
+            }
 
             if (TokenHandling.CreateToken(req.EmpNo))
             {
+                resModel.Status = STATUS.OK;
                 resModel.Token = empService.FetchEMPInfo_Service(req.EmpNo).First().ACCESS_TOKEN;
                 resModel.Authenticated = true;
+            }
+            else
+            {
+                Logger.WriteError("トークンの生成に失敗しました。");
+                return resModel;
             }
             return resModel;
         }
