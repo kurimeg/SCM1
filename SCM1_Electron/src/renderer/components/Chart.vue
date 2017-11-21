0f0c4c291159c1a43bf6cb30904dc936e742192c
Neo <33438006+NeoSanuki@users.noreply.github.com>
2017-11-21 10:10:51 +0900

    最新ＨＴＭＬの反映

--- SCM1_Electron/src/renderer/components/Chart.vue
+++ SCM1_Electron/src/renderer/components/Chart.vue
@@ -1,8 +1,14 @@
 <template>
 	<div class="main">
-		<div class="row01 floatL">
+		<div class="kensaku">
+			<form id="content">
+			  <input type="text" name="input" class="input">
+			  <button type="reset" class="search"></button>
+			</form>
+		</div>
+		<div class="row01 floatL child">
 			<div class="row01_seat_top">
-				<button type="button" class="seat_horizontal_pushed">中村</button>
+				<button type="button" class="seat_horizontal"></button>
 				<div class="desk_square"></div>
 			</div>
 			<div class="row01_seat_bottom">
@@ -30,7 +36,7 @@
 			</div>
 		</div>
 		<div class="rack01 floatL"></div>
-		<div class="row02 floatL">
+		<div class="row02 floatL child">
 			<div class="row02_seat_top">
 				<button type="button" class="seat_vertical floatL"></button>
 				<div class="desk_square floatL"></div>
@@ -63,7 +69,7 @@
 			</div>
 			<div class="rack02 floatL"></div>
 		</div>
-		<div class="row02 floatL">
+		<div class="row02 floatL child">
 			<div class="row02_seat_top">
 				<button type="button" class="seat_vertical floatL"></button>
 				<div class="desk_square floatL"></div>
@@ -96,7 +102,7 @@
 			</div>
 			<div class="rack02 floatL"></div>
 		</div>
-		<div class="row02 floatL">
+		<div class="row02 floatL child">
 			<div class="row02_seat_top">
 				<button type="button" class="seat_vertical floatL"></button>
 				<div class="desk_square floatL"></div>
@@ -129,7 +135,7 @@
 			</div>
 			<div class="rack02 floatL"></div>
 		</div>
-		<div class="row02 floatL">
+		<div class="row02 floatL child">
 			<div class="row02_seat_top">
 				<button type="button" class="seat_vertical floatL"></button>
 				<div class="desk_square floatL"></div>
@@ -160,7 +166,7 @@
 			</div>
 			<div class="rack02 floatL"></div>
 		</div>
-		<div class="row01 floatL">
+		<div class="row01 floatL child">
 			<div class="row01_seat_top">
 				<button type="button" class="seat_horizontal"></button>
 				<div class="desk_square"></div>
@@ -168,7 +174,7 @@
 			<div class="row06_seat_bottom">
 				<div class="line06 floatL">
 					<button type="button" class="seat_vertical"></button>
-					<button type="button" class="seat_vertical_pushed">渡邊</button>
+					<button type="button" class="seat_vertical"></button>
 					<button type="button" class="seat_vertical"></button>
 					<button type="button" class="seat_vertical"></button>
 					<button type="button" class="seat_vertical"></button>
@@ -190,7 +196,7 @@
 			</div>
 			<div class="rack02 floatL"></div>
 		</div>
-		<div class="row07 floatL">
+		<div class="row07 floatL child">
 			<div class="row07_seat_bottom">
 				<div class="line07 floatL">
 					<button type="button" class="seat_vertical"></button>
@@ -219,7 +225,7 @@
 			<div class="rack02 floatL"></div>
 		</div>
 		<!--正方形机-->
-		<div class="floatL">
+		<div class="floatL child">
 			<table>
 				<tbody>
 					<tr>
@@ -258,7 +264,7 @@
 						<td>
 							<div class="freeSpace">
 								<button type="button" class="seat_vertical square02_seat_left"></button>
-								<button type="button" class="seat_horizontal_pushed square02_seat_top">石塚</button>
+								<button type="button" class="seat_horizontal square02_seat_top"></button>
 								<div class="desk_square02">
 								</div>
 								<button type="button" class="seat_vertical square02_seat_right"></button>
@@ -314,7 +320,7 @@
 			</table>
 		</div>
 		<!--正方形机-->
-		<div class="nine_desk floatL">
+		<div class="nine_desk floatL child">
 			<table>
 				<tbody>
 					<tr>
@@ -416,22 +422,24 @@
 				</tbody>
 			</table>
 		</div>
+		<div id="minimap"></div>
 	</div>
 </template>
 
 <style>
     body {
-        margin: 0;
-        font-family: 'ＭＳ Ｐ明朝', 'MS PMincho','ヒラギノ明朝 Pro W3', 'Hiragino Mincho Pro', 'serif'sans-serif;
+	margin: 0;
+	font-family: 'ＭＳ Ｐ明朝', 'MS PMincho','ヒラギノ明朝 Pro W3', 'Hiragino Mincho Pro', 'serif'sans-serif;
     }
     .main {
-        width: 1410px;
-        margin: 50px auto;
+        width: 1500px;
+        height: 600px;
+        margin: 10px auto;
     }
     .floatL {
-        float: left;
+    	float: left;
     }
-
+    
     /*ラック*/
     .rack01 {
         margin: 137px 8px 2px 0;
@@ -445,7 +453,7 @@
         margin: 0 17.5px 0;
         background-color: #E1BE7E;
     }
-
+    
     /*Button_共通*/
     /*ボタン_横*/
     .seat_horizontal_pushed{
@@ -460,6 +468,7 @@
         border-radius: 10px;
         cursor: pointer;
         font-size: 14px;
+    	z-index: 999;
     }
     .seat_horizontal {
         margin: 0px;
@@ -473,6 +482,7 @@
         border-radius: 10px;
         cursor: pointer;
         font-size: 14px;
+    	z-index: 999;
     }
     /*ボタン_縦*/
     .seat_vertical_pushed {
@@ -487,8 +497,9 @@
         border-radius: 10px;
         cursor: pointer;
         font-size: 14px;
-        -webkit-text-orientation: sideways;
-        text-orientation: sideways;
+    	-webkit-text-orientation: sideways;
+    	text-orientation: sideways;
+    	z-index: 999;
     }
     .seat_vertical {
         margin: 5px 2px;
@@ -502,113 +513,114 @@
         border-radius: 10px;
         cursor: pointer;
         font-size: 14px;
+    	z-index: 999;
     }
     /*正方形周り_ボタン_横*/
     .seat_arround_square01 {
-        margin: 4px 4px 4px 12px;
+    	margin: 4px 4px 4px 12px;
     }
-
+    
     /*列＿1行目*/
     .row01 {
         margin: 32px 8px 0 0;
     }
     .row01_seat_top {
-        margin: 0 0 0 26px;
+    	margin: 0 0 0 26px;
     }
     .desk_square {
         margin: 2px;
         width: 45px;
         height: 50px;
-        background-color: #7EA88D;
+    	background-color: #7EA88D;
     }
     .desk_rec01 {
         margin: 20px 2px 2px;
         width: 45px;
         height: 390px;
-        background-color: #7EA88D;
+    	background-color: #7EA88D;
     }
     .line01 {
-        margin-top: 20px;
+    	margin-top: 20px;
     }
     /*列＿２行目以降*/
     .row02 {
         margin: 46px 8px 0 0;
     }
     .row02_seat_top {
-        margin: 10px 0 0 0;
+    	margin: 10px 0 0 0;
     }
     .row02_seat_bottom {
-        margin: 20px 0 0 0;
+    	margin: 20px 0 0 0;
     }
     .line02 {
-        margin-top: 25px;
+    	margin-top: 25px;
     }
     .desk_rec02 {
         margin: 20px 2px 2px;
         width: 45px;
         height: 450px;
-        background-color: #7EA88D;
+    	background-color: #7EA88D;
     }
-
+    
     /*列＿5行目*/
     .line05 {
-        margin-top: 80px;
+    	margin-top: 80px;
     }
     .desk_rec05 {
-        margin: 75px 2px 2px;
-        width: 45px;
-        height: 395px;
-        background-color: #7EA88D;
+    	margin: 75px 2px 2px;
+    	width: 45px;
+    	height: 395px;
+    	background-color: #7EA88D;
     }
     /*列＿6行目*/
     .line06 {
-        margin-top: 86px;
+    	margin-top: 86px;
     }
     .desk_rec06 {
-        margin: 81px 2px 2px;
-        width: 45px;
-        height: 395px;
-        background-color: #7EA88D;
+    	margin: 81px 2px 2px;
+    	width: 45px;
+    	height: 395px;
+    	background-color: #7EA88D;
     }
-
+    
     /*列＿7行目*/
     .row07 {
         margin: 46px 22px 0 0;
     }
     .row07_seat_top {
-        margin: 10px 0 0 0;
+    	margin: 10px 0 0 0;
     }
     .row07_seat_bottom {
-        margin: 20px 0 0 0;
+    	margin: 20px 0 0 0;
     }
     .line07 {
-        margin-top: 75px;
+    	margin-top: 75px;
     }
     .desk_rec07 {
-        margin: 71px 2px 2px;
-        width: 45px;
-        height: 450px;
-        background-color: #7EA88D;
+    	margin: 71px 2px 2px;
+    	width: 45px;
+    	height: 450px;
+    	background-color: #7EA88D;
     }
-
+    
     /*フリースペース_正方形*/
     .freeSpace {
-        width: 110px;
-        max-height: 130px;
-        position: relative;
-        margin-left: 8px;
+    	width: 110px;
+    	max-height: 130px;
+    	position: relative;
+    	margin-left: 8px;
     }
     .desk_square02 {
         position: relative;
         left: 25px;
-        margin: 2px;
-        width: 60px;
-        height: 60px;
-        background-color: #7EA88D;
+    	margin: 2px;
+    	width: 60px;
+    	height: 60px;
+    	background-color: #7EA88D;
     }
     .square02_seat_top {
         display: inherit;
-        margin-left: 30px;
+    	margin-left: 30px;
     }
     .square02_seat_right {
         position: relative;
@@ -625,6 +637,152 @@
         top: 86px;
     }
     .nine_desk {
-        margin: 138px 0 0;
+    	margin: 138px 0 0;
+    }
+    
+    /*search icon*/
+    .kensaku {
+        width: 200px;
+        height: 50px;
+        background-color: #fff;
+        overflow: hidden;
+        transition: all 0.5s ease;
+        position: fixed;
+        top: 10px;
+        left: 1150px;
+    }
+    #content {
+        position: absolute;
+        height: 50px;
+        width: 200px;
+        margin-left: 220px;
+        top: 100%;
+        left: 40%;
+        transform: translate(-50%, -50%);
+    }
+    
+    #content.on {
+      -webkit-animation-name: in-out;
+      animation-name: in-out;
+      -webkit-animation-duration: 0.7s;
+      animation-duration: 0.7s;
+      -webkit-animation-timing-function: linear;
+      animation-timing-function: linear;
+      -webkit-animation-iteration-count: 1;
+      animation-iteration-count: 1;
+    }
+    
+    input {
+        box-sizing: border-box;
+        width: 40px;
+        height: 40px;
+        border: 3px solid #222;
+        border-radius: 50%;
+        background: none;
+        color: #000;
+        font-size: 16px;
+        font-weight: 400;
+        font-family: Roboto;
+        outline: 0;
+        -webkit-transition: width 0.4s ease-in-out, border-radius 0.8s ease-in-out, padding 0.2s;
+        transition: width 0.4s ease-in-out, border-radius 0.8s ease-in-out, padding 0.2s;
+        -webkit-transition-delay: 0.4s;
+        transition-delay: 0.4s;
+        -webkit-transform: translate(-100%, -50%);
+        -ms-transform: translate(-100%, -50%);
+        transform: translate(-100%, -50%);
+    }
+    
+    .search {
+        background: none;
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        height: 50px;
+        width: 50px;
+        padding: 0;
+        border-radius: 100%;
+        outline: 0;
+        border: 0;
+        color: inherit;
+        cursor: pointer;
+        -webkit-transition: 0.2s ease-in-out;
+        transition: 0.2s ease-in-out;
+        -webkit-transform: translate(-100%, -50%);
+        -ms-transform: translate(-100%, -50%);
+        transform: translate(-100%, -50%);
+    }
+    
+    .search:before {
+        content: "";
+        position: absolute;
+        width: 10px;
+        height: 3px;
+        background-color: #222;
+        -webkit-transform: rotate(45deg);
+        -ms-transform: rotate(45deg);
+        transform: rotate(45deg);
+        margin-top: 5px;
+        margin-left: 5px;
+        -webkit-transition: 0.2s ease-in-out;
+        transition: 0.2s ease-in-out;
+    }
+    
+    .close {
+      -webkit-transition: 0.4s ease-in-out;
+      transition: 0.4s ease-in-out;
+      -webkit-transition-delay: 0.4s;
+      transition-delay: 0.4s;
+    }
+    
+    .close:before {
+        content: "";
+        position: absolute;
+        width: 27px;
+        height: 3px;
+        margin-top: -10px;
+        margin-left: -25px;
+        background-color: #222;
+        -webkit-transform: rotate(45deg);
+        -ms-transform: rotate(45deg);
+        transform: rotate(45deg);
+        -webkit-transition: 0.2s ease-in-out;
+        transition: 0.2s ease-in-out;
+    }
+    
+    .close:after {
+        content: "";
+        position: absolute;
+        width: 27px;
+        height: 3px;
+        background-color: #222;
+        margin-top: -10px;
+        margin-left: -25px;
+        cursor: pointer;
+        -webkit-transform: rotate(-45deg);
+        -ms-transform: rotate(-45deg);
+        transform: rotate(-45deg);
+    }
+    
+    .square {
+        box-sizing: border-box;
+        padding: 0 40px 0 10px;
+        width: 200px;
+        height: 50px;
+        border: 3px solid #222;
+        border-radius: 0;
+        background: none;
+        color: #000;
+        font-family: Roboto;
+        font-size: 16px;
+        font-weight: 400;
+        outline: 0;
+        -webkit-transition: width 0.4s ease-in-out, border-radius 0.4s ease-in-out, padding 0.2s;
+        transition: width 0.4s ease-in-out, border-radius 0.4s ease-in-out, padding 0.2s;
+        -webkit-transition-delay: 0.4s, 0s, 0.4s;
+        transition-delay: 0.4s, 0s, 0.4s;
+        -webkit-transform: translate(-100%, -50%);
+        -ms-transform: translate(-100%, -50%);
+        transform: translate(-100%, -50%);
     }
 </style>
