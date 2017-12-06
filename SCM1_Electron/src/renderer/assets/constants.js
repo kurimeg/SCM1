10f73c340335c5c3567d8eecc3dd77134364ffe4
NeoSanuki <sanuki-neo@ksc.co.jp>
2017-12-06 12:08:46 +0900

    constマージ

--- SCM1_Electron/src/renderer/assets/constants.js
+++ SCM1_Electron/src/renderer/assets/constants.js
@@ -2,8 +2,5 @@ export const STATUS_OK = 'OK'
 export const STATUS_TOKEN_ER = 'TOKEN_ER'
 export const MODAL_NM_ERROR = 'Error'
 export const MODAL_NM_ALERT = 'Alert'
-<<<<<<< HEAD
 export const MODAL_NM_REGALERT = 'regAlert'
-=======
 export const GUEST_USER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE1MTI1MjU4OTcsImV4cCI6MTUxMjUyOTQ5NywiaWF0IjoxNTEyNTI1ODk3LCJpc3MiOiJTQ01Jc3N1ZXIifQ.Vr-nDYkDce9xiEI-Vh2Ed2nymhYB-05sxVbg2S37HHg'
->>>>>>> 75f81c03dc23578f013e7c7e0cabbd333f57d65a
