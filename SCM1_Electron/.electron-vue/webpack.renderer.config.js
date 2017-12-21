bfe4a499a66c97c84abaee70c6d4c7c2d3c93735
Megumi Kurihara <pomme.c5.musique@gmail.com>
2017-12-21 12:33:54 +0900

    ESLint/karma追加

--- SCM1_Electron/.electron-vue/webpack.renderer.config.js
+++ SCM1_Electron/.electron-vue/webpack.renderer.config.js
@@ -31,6 +31,17 @@ let rendererConfig = {
   module: {
     rules: [
       {
+        test: /\.(js|vue)$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: {
+          loader: 'eslint-loader',
+          options: {
+            formatter: require('eslint-friendly-formatter')
+          }
+        }
+      },
+      {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
