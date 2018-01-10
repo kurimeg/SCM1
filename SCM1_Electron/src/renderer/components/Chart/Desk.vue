222237bcbce12aeddffcee6eeea8cf803793e6fa
sakurai_rina <r.sakurai13@gmail.com>
2018-01-10 11:41:36 +0900

    CNT側座席表追加

--- SCM1_Electron/src/renderer/components/Chart/Desk.vue
+++ SCM1_Electron/src/renderer/components/Chart/Desk.vue
@@ -1,18 +1,19 @@
 <template>
   <transition name="fade">
     <div>
-      <component :is="modalName"></component>
-      <desk-station-side></desk-station-side>
+      <!-- <component :is="modalName"></component> -->
+      <desk-sea-side></desk-sea-side>
     </div>
   </transition>
 </template>
 
 <script>
 import DeskStationSide from '@/components/Chart/DeskStationSide'
+import DeskSeaSide from '@/components/Chart/DeskSeaSide'
 
 export default {
   components: {
-    DeskStationSide
+    DeskStationSide, DeskSeaSide
   }
 }
 </script>
